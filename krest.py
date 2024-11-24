b0c57d7eb3c7d447e06f07504ab899540c5e1939
JohnStarfarer <181259590+JohnStarfarer@users.noreply.github.com>
2024-11-24 20:04:51 +0500

    изменил то что нужно изменить; убрал то что нужно убрать

--- krest.py
+++ krest.py
@@ -1,4 +1,8 @@
-def check_winner(board):
+board = [['.','.','.'], #глобальный массив состояния поля
+         ['.','.','.'],
+         ['.','.','.']]
+
+def Check_Winner(): #функция проверки победы
     # Проверка строк
     for row in board:
         if row[0] == row[1] == row[2] == "X" or row[0] == row[1] == row[2] == "0":
@@ -18,40 +22,57 @@ def check_winner(board):
 
     return False
 
-def vivod(board):
-    for a in board:
-        print('|'.join(a))
+def PrintBoard(): #функция вывода поля
+    for el in board:
+        print('|'.join(el))
 
-def proverka(x,y,board):
-    if x<3 and x>=0 and y<3 and y>=0 and board[x][y] == '.' :
-        return True
+# def proverka(x,y):
+#     if x<3 and x>=0 and y<3 and y>=0 and board[x][y] == '.' :
+#         return True
+#     else:
+#         return False
+    
+def Move(current_player): #функция хода текущего игрока
+    print(f'Cейчах ходят - {current_player}')
+    input_player = True #флаг для ввода координат
+    break_game = False #флаг для прекращения игры (try-false не позволяют закрыть терминал Ctrl^C) (exit() тоже не помогает)
+    while input_player:
+        try:
+            PrintBoard()
+            xy = input('Введите номер строки и столбца (для выхода введите 0): ')
+            if xy == '0':
+                input_player = False
+                break_game = True
+            else:
+                x, y = map(int, xy.split())
+                x -= 1
+                y -= 1
+                if board[x][y] != '.':
+                    print('\tЭта ячейка занята!')
+                else: input_player = False
+        except:
+            print('\tНеверный ввод!')
+    print() #отступ, если ход верный
+    if break_game: return 'BREAK' #exit() не работает
+    board[x][y] = current_player
+
+    if Check_Winner(): #победил данный игрок в этом ходе или нет
+        return current_player
     else:
         return False
 
+#основной блок кода
 print('Игра крестики-нолики!!!')
-maps = [['.','.','.'],['.','.','.'],['.','.','.']]
-i = 1
 current_player = 'X'
-vivod(maps)
+winner = False
 
-while i != 9:
-    print(f'Cейчах ходят - {current_player}')
-    x, y = map(int,input('введите координаты x(0-2),y(0,2) - ' ).split())
-    #проверяем правильность ввода
-    if proverka(x,y,maps):
-        maps[x][y] = current_player
-        vivod(maps)
-        #меняем ход
-        current_player = '0' if current_player == 'X' else 'X'
-        i +=1
-    else:
-        print('Неверный ввод')
-    #проверяем условие победы
-    if check_winner(maps):
-        current_player = '0' if current_player == 'X' else 'X'
-        print(f'Выйграли! - {current_player}')
+while '.' in [el for el_list in board for el in el_list] and not(winner): #пока существуют "пустые" ячейки и нет победителя / итерация
+    winner = Move(current_player)
+    if winner == 'BREAK':
         break
+    elif winner:
+        print(f'Выигрывает {winner}!')
+    else: current_player = '0' if current_player == 'X' else 'X'
 
-if check_winner(maps) == False:
+if not(Check_Winner()):
     print('Ничья!')
-
